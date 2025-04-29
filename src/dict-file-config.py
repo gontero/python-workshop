b60f85052a63a571c3c26b4f6d64e306594b1db7
RobertRyba <robert.ryba@auto1.com>
2025-04-29 13:23:44 +0200

    Create some basic logs

--- src/dict-file-config.py
+++ src/dict-file-config.py
@@ -1,6 +1,7 @@
 import json
 import logging.config
 import pathlib
+import json_log_formatter
 
 logger = logging.getLogger("my_app") # use My own logger
 
@@ -9,11 +10,16 @@ def setup_logging():
     with open(config_file) as f_in:
         config = json.load(f_in)
     logging.config.dictConfig(config)
+    handler = logging.StreamHandler()
+    formatter = json_log_formatter.JSONFormatter()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
 
 def main():
     setup_logging()
     # logging.info("some mesage") avoid to use it
-    logger.debug("This is a debug message")
+    logger.debug("This is a debug message", extra={"extra": "field"})
     logger.info("This is an info message")
     logger.warning("This is a warning message")
     logger.error("This is an error message")
